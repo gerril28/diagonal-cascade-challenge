a192b8a430b539f43a5b3908cc54aece15f02c72
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-11-06 00:44:13 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,23 @@
 input.onButtonPressed(Button.A, function () {
     for (let x = 0; x <= 4; x++) {
         for (let y = 0; y <= 4; y++) {
-            led.toggle(y, 0 - y)
+            led.toggle(x, y - 0)
             basic.pause(100)
-            led.toggle(y, 1 - y)
+            led.toggle(x, 1 - y)
             basic.pause(100)
-            led.toggle(y, 2 - y)
+            led.toggle(y, 2 - x)
             basic.pause(100)
             led.toggle(y, 3 - y)
+            basic.pause(100)
+            led.toggle(y, 4 - y)
+            basic.pause(100)
+            led.toggle(y, 5 - y)
+            basic.pause(100)
+            led.toggle(y, 6 - y)
+            basic.pause(100)
+            led.toggle(y, 7 - y)
+            basic.pause(100)
+            led.toggle(y, 8 - y)
         }
     }
 })
